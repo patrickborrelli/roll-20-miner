d2d638bed65f32c004084157528998cc1c64b462
patrick borrelli <patrickborrelli@gmail.com>
2025-09-09 16:50:21 -0400

    non-working update

--- src/main/java/com/patrickborrelli/roll20miner/Roll20LogMiner.java
+++ src/main/java/com/patrickborrelli/roll20miner/Roll20LogMiner.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import com.patrickborrelli.roll20miner.model.Message;
